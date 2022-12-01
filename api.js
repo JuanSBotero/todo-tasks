973b14dcc22e64f91a0c89aa62e1d0c3d6b3cf00
Juan Sebastian Botero <96149549+JuanSBotero@users.noreply.github.com>
2022-12-01 15:28:51 -0500

    Create task_schema

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var express = require("express");
+var TaskModel = require('./task_schema');
 var router = express.Router();
 var query = "mongodb+srv://juan_user:1234@cluster0.cfspl3v.mongodb.net/taskBD?retryWrites=true&w=majority"
 const db = (query);
