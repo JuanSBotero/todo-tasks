62026a2aa1585b5bc8775d3d7bb2653ae5efa522
Juan Sebastian Botero <96149549+JuanSBotero@users.noreply.github.com>
2022-12-01 16:41:03 -0500

    Api.js, server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const port = process.env.PORT || 3000;
 const app = express();
 const api = require('./api');
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
 
 app.use(bodyParser.json());
 
